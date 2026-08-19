c8758671f0fa1b9f7286a308683926cd363c0882
Josue Almaraz <almaraz5josue@gmail.com>
2026-08-19 16:43:48 -0500

    finish quick start section

--- src/components/QuickStart.jsx
+++ src/components/QuickStart.jsx
@@ -1,12 +1,13 @@
 import "./QuickStart.css";
+import { Dumbbell, Heart, PersonStanding, Plus } from "lucide-react";
 
 function QuickStart() {
 
     const exercise = [
-        {name: "Strength", icon: "🏋️"},
-        {name: "Cardio", icon: "🏃"},
-        {name: "Mobility", icon: "🧘"},
-        {name: "Other", icon: "+"}
+        { name: "Strength", icon: Dumbbell, iconClass: "strength-icon"},
+        { name: "Cardio", icon: Heart, iconClass: "cardio-icon" },
+        { name: "Mobility", icon: PersonStanding, iconClass: "mobility-icon" },
+        { name: "Other", icon: Plus, iconClass: "other-icon" }
     ];
 
     return (
@@ -15,18 +16,20 @@ function QuickStart() {
                 <h2>Quick Start</h2>
                 <button className="quick-start-see-all">See all</button>
             </div>
-            
-            <div className="quick-start-exercise"> 
-                {exercise.map((exercise) => (
-                    <button className="quick-start-card">
-                        <span>{exercise.icon}</span>
-                        <p>{exercise.name}</p>
-                    </button>
-                ))}
 
+            <div className="quick-start-exercise">
+                {exercise.map((exercise) => {
+                    const Icon = exercise.icon;
 
+                    return (
+                        <button className="quick-start-card" key={exercise.name}>
+                            <Icon className={exercise.iconClass}/>
+                            <span>{exercise.name}</span>
+                        </button>
+                    );
+                })}
             </div>
-            
+
         </section>
 
 
