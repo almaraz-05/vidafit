e2630b7d3ad1cacc7aa3b5c5ae3c70463b9382e2
Josue Almaraz <almaraz5josue@gmail.com>
2026-08-19 23:19:56 -0500

    finish recent activity section

--- src/components/RecentActivity.jsx
+++ src/components/RecentActivity.jsx
@@ -1,12 +1,12 @@
-import { ChevronRight } from "lucide-react";
+import { ChevronRight, Dumbbell, Heart } from "lucide-react";
 import "./RecentActivity.css";
 
 function RecentActivity() {
 
     const activities = [
-        { name: "Upper", date: "Today", exercises: "6 exercises", time: "1hr 15min" },
-        { name: "Lower", date: "August 17", exercises: "5 exercises", time: "1 hr 5 min" },
-        { name: "Cardio", date: "August 15", distance: "4 miles", time: "42 min" }
+        { name: "Upper",  icon: Dumbbell, iconClass: "upper-activtiy-icon",  date: "Today",     exercises: "6 exercises", time: "1hr 15min" },
+        { name: "Lower",  icon: Dumbbell, iconClass: "lower-activity-icon",  date: "August 17", exercises: "5 exercises", time: "1 hr 5 min" },
+        { name: "Cardio", icon: Heart,    iconClass: "cardio-activity-icon", date: "August 15", distance: "4 miles", time: "42 min" }
     ];
 
     return (
@@ -16,19 +16,27 @@ function RecentActivity() {
                 <button className="recent-activity-see-all">See all</button>
             </div>
 
-            {activities.map((activity) => (
-                <div className="recent-activity-card" key={activity.name}>
-                    <div className="recent-activity-icon"></div>
+            {activities.map((activity) => {
+                const Icon = activity.icon;
 
-                    <div className="recent-activity-info"></div>
-
-                    <div className="recent-activity-chevron">
-                        <ChevronRight />
-                    </div>
-                </div>
+                return (
+                    <button className="recent-activity-card" key={activity.name}>
+                        <div className={`recent-activity-icon ${activity.iconClass}`}>
+                            <Icon />
+                        </div>
 
 
-            ))}
+                        <div className="recent-activity-info">
+                            <h3>{activity.name}</h3>
+                            <p>{activity.date} • {activity.exercises || activity.distance} • {activity.time}</p>
+                        </div>
+
+                        <div className="recent-activity-chevron">
+                            <ChevronRight />
+                        </div>
+                    </button>
+                );
+            })}
 
 
         </section>
