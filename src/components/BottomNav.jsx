2ea30e73826bb7c1f458dca0da83cf330b9eba29
Josue Almaraz <almaraz5josue@gmail.com>
2026-08-21 21:14:02 -0500

    finish bottom nav bar

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,39 +1,31 @@
+import { Home, CirclePlay, ChartNoAxesCombined, User } from 'lucide-react';
 import "./BottomNav.css";
 
 function BottomNav() {
 
     const navOptions = [
-        {},
-        {},
-        {},
-        {}
-    ]
+        {name: "Home", icon: Home, iconClass: "home-icon"},
+        {name: "Workouts", icon: CirclePlay, iconClass: "workouts-icon"},
+        {name: "Progress", icon: ChartNoAxesCombined, iconClass: "progress-icon"},
+        {name: "Profile", icon: User, iconClass: "profile-icon"}
+    ];
 
 
     return (
         <nav className="bottom-nav">
+            {navOptions.map((option) => {
+                const Icon = option.icon;
 
-            <button className="bottom-nav-info">
-                {/* <Home /> */}
-                <span>Home</span>
-            </button>
+                return (
+                    <button className="bottom-nav-info" key={option.name}>
+                        <Icon className={option.iconClass}/>
+                        <span>{option.name}</span>
+                    </button>
 
+                );
 
-            <button className="bottom-nav-info">
-                {/* <Home /> */}
-                <span>Workouts</span>
-            </button>
+            })}
 
-
-            <button className="bottom-nav-info">
-                {/* <Home /> */}
-                <span>Progress</span>
-            </button> 
-
-            <button className="bottom-nav-info">
-                {/* <Home /> */}
-                <span>Profile</span>
-            </button>
         </nav>
     );
 }
